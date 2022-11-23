75aefa01f48b321eddd241ef3c3ab45b82365572
vishnunani053 <vishnulucky053@gmail.com>
2022-11-24 01:09:06 +0530

    okk

--- InbuiltModules/httpServer/server.js
+++ InbuiltModules/httpServer/server.js
@@ -48,24 +48,24 @@ console.log(`server is running...${PORT}`)
 })
  */
 
-const http=require('http')
-const path=require('path')
-const dotenv=require('dotenv');
+const http = require('http')
+const path = require('path')
+const dotenv = require('dotenv');
 const { env } = require('process');
 
 //let server=http.createServer().listen()
 
-let filePath=path.join(__dirname,'config','config.env')
+let filePath = path.join(__dirname, 'config', 'config.env')
 console.log(filePath);
-dotenv.config({path:filePath})
+dotenv.config({ path: filePath })
 
-let PORT=process.env.PORT
+let PORT = process.env.PORT
 
-let server=http.createServer((request,response)=>{
-response.writeHead(200,{'content-type':'text/html'})
-response.end('<h1>heloo good mornig rahul gi</h1>')
+let server = http.createServer((request, response) => {
+    response.writeHead(200, { 'content-type': 'text/html' })
+    response.end('<h1>heloo good mornig rahul gi</h1>')
 })
 
-server.listen(PORT,()=>{
-    console.log(`server is running..... ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`server is running....... ${PORT}`);
 })
