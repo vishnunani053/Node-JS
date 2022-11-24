ad46dc855c15286f1735d47805d04e3658b4e1aa
vishnunani053 <vishnulucky053@gmail.com>
2022-11-24 16:22:38 +0530

    okkie

--- InbuiltModules/httpServer/server.js
+++ InbuiltModules/httpServer/server.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/* const http = require('http')
 const dotenv = require('dotenv')
 const path = require('path')
 const url = require('url')
@@ -48,4 +48,53 @@ let server = http.createServer((request, response) => {
 
 server.listen(PORT, () => {
     console.log(`server is running on.......http://localhost:${PORT}`)
+})
+ */
+
+
+const http = require('http')
+const path = require('path')
+const dotenv = require('dotenv')
+const fs = require('fs')
+const url = require('url')
+
+let filePath = path.join(__dirname, 'config', 'config.env')
+dotenv.config({ path: filePath })
+
+let PORT = process.env.PORT
+
+//let server=http.createServer().listen()
+
+let server = http.createServer((request, response) => {
+    let url = request.url
+    if (url == '/') {
+        fs.readFile('./views/index.html', 'utf-8', (err, data) => {
+            if (err) throw err
+            response.end(data)
+        })
+    }
+    if (url == '/about') {
+        fs.readFile('./views/about.html', 'utf-8', (err, data) => {
+            if (err) throw err
+            response.end(data)
+        })
+    }
+    if (url == '/contact') {
+        fs.readFile('./views/contact.html', 'utf-8', (err, data) => {
+            if (err) throw err
+            response.end(data)
+        })
+
+    }
+    if (url == '/services') {
+        fs.readFile('./views/services.html', 'utf-8', (err, data) => {
+            if (err) throw err
+            response.end(data)
+        })
+
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`server is running...http://localhost:${PORT}`);
 })
